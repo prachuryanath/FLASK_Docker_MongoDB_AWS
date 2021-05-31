504b33a0e979d69eed34c9b77a6a6fc2b452b468
Prachurya Nath <prachuryanath00@gmail.com>
2021-05-31 20:50:19 +0530

    Unit 1

--- Unit_1/app.py
+++ Unit_1/app.py
@@ -13,11 +13,32 @@ JSON :
     "field2":"def"
 }
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
+def hi_there_everyone():
+    return "Home page"
+
+@app.route('/add_two_nums', methods=["POST", "GET"])
+def add_two_nums():
+    # Get x and y from the posted data
+    dataDict = request.get_json()
+    if "y" not in dataDict:
+        return "ERROR", 305
+    x = dataDict["x"]
+    y = dataDict["y"]
+    # Add x and y
+    z = x + y
+    #Prepare a JSON
+    retJSON = {
+        "z":z
+    }
+    #return jsonify(map_prepared)
+    return jsonify(retJSON), 200
+
+@app.route('/bye')
 def hello_world():
     jfun = {
     "field 1":"abc",
